b9cefab32915f14a07b465e957423767318d9a5e
zzhukanton <zzhukanton@gmail.com>
2021-06-23 00:54:05 +0300

    Добавьте файлы проекта.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LegacyFoldering.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LegacyFoldering.Startup))]
+namespace LegacyFoldering
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
