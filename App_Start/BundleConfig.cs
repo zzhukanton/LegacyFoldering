3b71ba91fca960ed09d49615eb070ccdf06ca0bc
zzhukanton <zzhukanton@gmail.com>
2021-06-24 23:38:41 +0300

    add react;v additional cleanup

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -25,6 +25,10 @@ namespace LegacyFoldering
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
 					  "~/Content/site.css"));
+
+			bundles.Add(new ScriptBundle("~/bundles/react")
+				.Include("~/Scripts/react/umd/react.development.js")
+				.Include("~/Scripts/react-dom/umd/react-dom.development.js"));
 		}
 	}
 }
