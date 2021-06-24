4c83387ec80bf9094b23a89bd38790c58615d3d9
zzhukanton <zzhukanton@gmail.com>
2021-06-25 00:48:54 +0300

    add root endpoint

---
+++ Controllers/FolderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LegacyFoldering.Controllers
+{
+    public class FolderController : Controller
+    {
+        // GET: Root
+        [HttpGet]
+        public JsonResult Root()
+        {
+            string[] data = Directory.GetFileSystemEntries(@"D:\");
+
+            return Json(data);
+        }
+    }
+}
